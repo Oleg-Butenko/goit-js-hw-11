f15e92edc254dfa59f27d31b1d130b6aa858177b
Oleh Butenko <butenkoleh@gmail.com>
2025-07-22 18:54:32 +0300

    fix js files

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,37 +1,35 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 const gallery = document.querySelector(".gallery");
-const loader = document.querySelector(".loader")
+const loader = document.querySelector(".loader");
+const simpleGallery = new SimpleLightbox('.gallery a', {});
 
 export function createGallery(images) {
     gallery.innerHTML = images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => 
-        `
-    <li class="gallery-item">
-    <a class="gallery-link" href="${largeImageURL}">
-        <img class="gallery-img" src="${webformatURL}" alt="${tags}"/>
-        </a>
+        `<li class="gallery-item">
+            <a class="gallery-link" href="${largeImageURL}">
+                <img class="gallery-img" src="${webformatURL}" alt="${tags}"/>
+            </a>
             <ul class="descr-list">
                 <li class="descr-list-item">
-                <h2>Likes</h2>
-                <p>${likes}</p>
+                    <h2>Likes</h2>
+                    <p>${likes}</p>
                 </li>
                 <li class="descr-list-item">
-                <h2>Views</h2>
-                <p>${views}</p>
+                    <h2>Views</h2>
+                    <p>${views}</p>
                 </li>
                 <li class="descr-list-item">
-                <h2>Comments</h2>
-                <p>${comments}</p>
+                    <h2>Comments</h2>
+                    <p>${comments}</p>
                 </li>
                 <li class="descr-list-item">
-                <h2>Downloads</h2>
-                <p>${downloads}</p>
+                    <h2>Downloads</h2>
+                    <p>${downloads}</p>
                 </li>
             </ul>
-        </li>
-        `
+        </li>`
     ).join("")
-    let simpleGallery = new SimpleLightbox('.gallery a', {});
     simpleGallery.refresh();
 }
 
--- src/main.js
+++ src/main.js
@@ -1,18 +1,45 @@
 import getImagesByQuery from "./js/pixabay-api"
-const form = document.querySelector(".form");
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-import { clearGallery, showLoader,  } from "./js/render-functions"
+import { clearGallery, showLoader, createGallery, hideLoader } from "./js/render-functions"
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
+const form = document.querySelector(".form");
 form.addEventListener("submit", handleSubmit);
 
-
-
 function handleSubmit(event) {
-    event.preventDefault();
-    clearGallery()
+    event.preventDefault();   
+    clearGallery();
     showLoader();
-    const [input] = event.target.elements
-    getImagesByQuery(input.value);
+    const [input] = event.target.elements;
+    if (!input.value.trim()) {
+        iziToast.show({
+                message: "Please enter a valid search query",
+                 messageColor: 'white',
+                backgroundColor: 'red',
+                maxWidth: "432px",
+                close: true,
+            position: "topRight",
+        }
+        )
+        hideLoader();
+        form.reset();
+         return
+    }
+    getImagesByQuery(input.value).then(res => {
+        if (res.data.hits.length === 0) {
+            iziToast.show({
+                message: "Sorry, there are no images matching your search query. Please try again!",
+                 messageColor: 'white',
+                backgroundColor: 'red',
+                maxWidth: "432px",
+                close: true,
+                position: "topRight",
+})
+        } else {
+            createGallery(res.data.hits);      
+        }
+        hideLoader();
+    }
+    )
     form.reset();
 }
